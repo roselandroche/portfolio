b4e21512a3848c71acd12298e292d1b91773b20e
roselandroche <r.landroche@gmail.com>
2020-03-05 20:22:37 -0500

    Adding styles

--- my-portfolio/src/components/Skills.js
+++ my-portfolio/src/components/Skills.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import NavBar from './NavBar';
+import '../css/Skills.css';
 
 function Skills() {
     return (
-        <div>
+        <>
             <NavBar />
-            <h1>Technical Skills</h1>
-            <h3>Node.js</h3>
-            <h3>SQL, SQLite</h3>
-            <h3>Express</h3>
-            <h3>Knex.js</h3>
-            <h3>Redux</h3>
-            <h3>React.js</h3>
-            <h3>JavaScript</h3>
-            <h3>CSS, Less, Sass</h3>
-            <h3>HTML</h3>
-        </div>
+            <div className='skills'>
+                <h1>Technical Skills</h1>
+                <h3>Node.js</h3>
+                <h3>SQL, SQLite</h3>
+                <h3>Express</h3>
+                <h3>Knex.js</h3>
+                <h3>Redux</h3>
+                <h3>React.js</h3>
+                <h3>JavaScript</h3>
+                <h3>CSS, Less, Sass</h3>
+                <h3>HTML</h3>
+            </div>
+        </>
     )
 }
 
