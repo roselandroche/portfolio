d5ec5f7910e18d596efd80d6167096def1bb2be5
roselandroche <r.landroche@gmail.com>
2020-03-23 14:43:59 -0400

    Deleting excess, changing logo

--- my-portfolio/src/components/NavBar.js
+++ my-portfolio/src/components/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import '../css/NavBar.css'
+import '../css/NavBar.css';
+import favicon from '../favicon.png'
 
 function NavBar() {
     return (
         <div className="nav-container">
-            <Link exact to='/' className="nav-link">HOME</Link>
+            <Link exact to='/' >
+                <img src={favicon} alt='logo' className='' />
+            </Link>
             <Link to='/bio' className="nav-link">BIO</Link>
             <Link to='/skills' className="nav-link">SKILLS</Link>
             <Link to='/projects' className="nav-link">PROJECTS</Link>
