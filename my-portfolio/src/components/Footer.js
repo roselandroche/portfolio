89d727736ec36d241ce93d9308358adc334838b7
roselandroche <r.landroche@gmail.com>
2020-03-27 15:50:19 -0400

    Fixed links in Footer

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -1,27 +1,21 @@
-import React, { Match, Miss } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import TwitterIcon from '@material-ui/icons/Twitter';
-// import Home from './Home';
 import '../css/Footer.css';
 
 function Footer() {
     return (
         <div className='prof-links'>
-            {/* <Match exactly pattern="/github" component={() => window.location = 'https://github.com/roselandroche'} >
+            <a href='https://github.com/roselandroche' className='media'>
                 <GitHubIcon />
-            </Match>
-            <Miss component={ Home } /> */}
-            <Link to='https://github.com/roselandroche' className='media'>
-                <GitHubIcon />
-            </Link>
-            <Link to='https://www.linkedin.com/in/rose-landroche-8882b09b/' className='media'>
+            </a>
+            <a href='https://www.linkedin.com/in/rose-landroche-8882b09b/' className='media'>
                 <LinkedInIcon />
-            </Link>
-            <Link to='https://twitter.com/creatively_rose' className='media'>
+            </a>
+            <a href='https://twitter.com/creatively_rose' className='media'>
                 <TwitterIcon />
-            </Link>
+            </a>
         </div>
     )
 }
