51335ca355b7a7b331aeea54feb7b79bb1d43d76
roselandroche <r.landroche@gmail.com>
2020-04-11 14:11:42 -0400

    Adding Project 2 content

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -5,17 +5,23 @@ function Projects() {
     return (
         <div>
             <NavBar />
-            <h1>Project 1: Gardening App Marketing Page</h1>
+            <h1>Project 1: Water My Plants</h1>
             <p>
-                A landing page utilizing CSS, LESS, HTML, and JavaScript.
+                A landing page utilizing CSS, LESS, HTML, and JavaScript showcasing an application to
+                help users care for plants.
             </p>
             <a href='https://hopeful-villani-ed8e0d.netlify.com/index.html'>
-                <img src = '../WaterPlants.png' alt = 'Screenshot of landing page'></img>
+                <p>Deployed Site</p>
             </a>
 
-            <h1>Project 2</h1>
-            <p>Concise Description</p>
-            <p>Deployed Link</p>
+            <h1>Project 2: Pintereach</h1>
+            <p>
+                An application built with a cross functional team, utilizing React 
+                in order to give users the ability to save articles to be read at a later date.
+            </p>
+            <a href='https://eloquent-mcnulty-a0dd5b.netlify.com/'>
+                <p>Deployed Site</p>
+            </a>
         </div>
     )
 }
