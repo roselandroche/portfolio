aebcdbbede0b6d9edd21b78534e64a43124b1007
roselandroche <r.landroche@gmail.com>
2020-04-11 13:38:40 -0400

    Updated Project 1 content

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -5,9 +5,12 @@ function Projects() {
     return (
         <div>
             <NavBar />
-            <h1>Project 1</h1>
-            <p>Concise Description</p>
-            <p>Deployed Link</p>
+            <h1>Project 1: Marketing Page for Gardening Application</h1>
+            <p>
+                A landing page utilizing CSS, LESS, HTML, and JavaScript languages to create a 
+                functional and beautiful site..
+            </p>
+            <p>https://hopeful-villani-ed8e0d.netlify.com/index.html#</p>
 
             <h1>Project 2</h1>
             <p>Concise Description</p>
