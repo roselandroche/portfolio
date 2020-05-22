3db8ed5689779af88a8745ba98daa7828ceb7dec
roselandroche <r.landroche@gmail.com>
2020-05-21 21:33:43 -0400

    Changed structure of application

---
+++ build/precache-manifest.56d647e326878cfdf31c3354f81687ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b643ef2a88a6519ae421acb4846b6cb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30efefe92e27a571f4c1",
+    "url": "/static/css/main.6567bbae.chunk.css"
+  },
+  {
+    "revision": "d9f0e1d8a6a625cbe44a",
+    "url": "/static/js/2.3f81ef97.chunk.js"
+  },
+  {
+    "revision": "6527089adf7fbe9c7cd3f256d5c1a5ae",
+    "url": "/static/js/2.3f81ef97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30efefe92e27a571f4c1",
+    "url": "/static/js/main.538ca286.chunk.js"
+  },
+  {
+    "revision": "4230ff567fdbcbce86f0",
+    "url": "/static/js/runtime-main.d97d6f01.js"
+  },
+  {
+    "revision": "65ae752a2c5c2170a805326e429c50a2",
+    "url": "/static/media/Pintereach.65ae752a.png"
+  },
+  {
+    "revision": "05676d5a5492e3e7daf2c9c6042dadc8",
+    "url": "/static/media/WaterPlants.05676d5a.png"
+  }
+]);
