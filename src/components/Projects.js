ff94f0e93ce987b17b134d7e4ea0bdc3f1f125f1
roselandroche <r.landroche@gmail.com>
2020-05-29 18:51:03 -0400

    Content edits to Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,27 +11,18 @@ function Projects() {
         <div>
             <NavBar />
             <div className='projectCard'>
-                <h1>Project 1: Water My Plants</h1>
+                <h1>Save This Job</h1>
                 <p>
-                    A landing page utilizing CSS, LESS, HTML, and JavaScript showcasing an application to
-                    help users care for plants.
+                    A back end for an application built with a cross functional team, utilizing Node.js and Knex
+                    in order to give users the ability to save job postings and related information as they search
+                    for their new career. 
                 </p>
-                <a href='https://hopeful-villani-ed8e0d.netlify.com/index.html'>
-                    <img src={WaterPlants} alt='Screenshot of landing page' className='screenshot' />
+                <a href='https://savethisjob.com'>
+                    <img src={SaveThisJob} alt='Screenshot of database' className='screenshot' />
                 </a>
             </div>
             <div className='projectCard'>
-                <h1>Project 2: Pintereach</h1>
-                <p>
-                    An application built with a cross functional team, utilizing React and Redux
-                    in order to give users the ability to save articles to be read at a later date.
-                </p>
-                <a href='https://eloquent-mcnulty-a0dd5b.netlify.com/'>
-                    <img src={Pintereach} alt='Screenshot of application' className='screenshot' />
-                </a>
-            </div>
-            <div className='projectCard'>
-                <h1>Project 3: Build Sauti Studio</h1>
+                <h1>Build Sauti Studio</h1>
                 <p>
                     A back end for an application built with a cross functional team, utilizing Node.js and Knex
                     in order to give users the ability to save and share technical projects with like-minded
@@ -42,14 +33,23 @@ function Projects() {
                 </a>
             </div>
             <div className='projectCard'>
-                <h1>Project 4: Save This Job</h1>
+                <h1>Pintereach</h1>
                 <p>
-                    A back end for an application built with a cross functional team, utilizing Node.js and Knex
-                    in order to give users the ability to save job postings and related information as they search
-                    for their new career. 
+                    An application built with a cross functional team, utilizing React and Redux
+                    in order to give users the ability to save articles to be read at a later date.
                 </p>
-                <a href='https://savethisjob.com'>
-                    <img src={SaveThisJob} alt='Screenshot of database' className='screenshot' />
+                <a href='https://eloquent-mcnulty-a0dd5b.netlify.com/'>
+                    <img src={Pintereach} alt='Screenshot of application' className='screenshot' />
+                </a>
+            </div>
+            <div className='projectCard'>
+                <h1>Water My Plants</h1>
+                <p>
+                    A landing page utilizing CSS, LESS, HTML, and JavaScript showcasing an application to
+                    help users care for plants.
+                </p>
+                <a href='https://hopeful-villani-ed8e0d.netlify.com/index.html'>
+                    <img src={WaterPlants} alt='Screenshot of landing page' className='screenshot' />
                 </a>
             </div>
         </div>
