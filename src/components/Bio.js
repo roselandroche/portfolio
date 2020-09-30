e5763c829435f8a6139c316a6b024df2cf6b8677
roselandroche <r.landroche@gmail.com>
2020-09-30 14:17:24 -0400

    Edits to Bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,20 +6,30 @@ function Bio() {
     return (
         <div>
             <NavBar />
-            <div className='bio'>
-                <h1>A little about me:</h1>
-                <p>
-                    My name is Rose Landroche and I am a Full Stack Web Developer. I have over ten years of experience
-                    in customer facing industries such as retail and customer service. I began learning to program on
-                    my own in 2018, and pursued organized learning through Lambda School in 2019. Programming has become 
-                    one of the ways I channel my appreciation for logic, as well as creating new things. I look forward
-                    to learning more new technologies and creating spectacular new things over the course of my career.
-                </p>
-                {/* <h1>Mission Statement:</h1>
-                <p>
-                    To actively improve the world, by working for and with like-minded 
-                    people, so that I leave the world better than I found it.
-                </p> */}
+            <div className='cards'>
+                <div className='mission'>
+                    <h1>Mission Statement:</h1>
+                    <p className='bio-p'>
+                        To continually learn, grow, and live with integrity.
+                    </p>
+                </div>
+                <div className='bio'>
+                    <h1>About me:</h1>
+                    <p className='bio-p'>
+                        My name is Rose Landroche. I am a Full Stack Web Developer based out of Vancouver British Columbia. 
+                    </p>
+                    <p className='bio-p'>
+                        I have over ten years of experience in customer service industries, growing soft skills such as communication
+                        and teamwork.
+                    </p>
+                    <p className='bio-p'>
+                        I began learning to program on my own in 2018, and pursued organized learning through Lambda School in 2019. 
+                    </p>
+                    <p className='bio-p'>
+                        Programming has become one of the ways I channel my appreciation for logic and creativity. I look forward
+                        to learning more new technologies and creating spectacular new things over the course of my career.
+                    </p>
+                </div>
             </div>
         </div>
     )
